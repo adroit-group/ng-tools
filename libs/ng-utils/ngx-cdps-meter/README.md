cd04d50fe8fbbba68cad3b3306ed29aa7a24feeb
Ferenczfi Jonatán <jonatanferenczfi@gmail.com>
2022-12-09 08:52:29 +0100

    feat: Remove old ngx-cdps-meter folder from under ng-utils

--- libs/ng-utils/ngx-cdps-meter/README.md
+++
@@ -1,3 +0,0 @@
-# @adroit-group/ng-utils/ngx-cdps-meter
-
-Secondary entry point of `@adroit-group/ng-utils`. It can be used by importing from `@adroit-group/ng-utils/ngx-cdps-meter`.
--- libs/ng-utils/ngx-cdps-meter/src/lib/ngx-cdps-meter.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { CdpsComponent } from './cdps/cdps.component';
-
-@NgModule({
-  imports: [CommonModule],
-  declarations: [CdpsComponent],
-})
-export class NgxCdpsMeterModule {}
