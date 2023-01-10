8c414f4ff9a4c41d609bbfea9a380e1d3365c5d7
Ferenczfi Jonatán <jonatanferenczfi@gmail.com>
2023-01-10 14:06:58 +0100

    fix: change ng rendering directive's platform input's type to a const

--- libs/ng-utils/src/lib/constants/application-platform.ts
+++ libs/ng-utils/src/lib/constants/application-platform.ts
@@ -2,9 +2,9 @@
  * An enum that specifies the platforms where an Angular application can be run.
  * The enum values correspond to the values of the platform id tokens that the Angular DI system can resolve.
  */
-export const enum EApplicationPlatform {
-  Browser = 'browser',
-  Server = 'server',
-  WorkerApp = 'browserWorkerApp',
-  WorkerUI = 'browserWorkerUi',
-}
+export const EApplicationPlatform = {
+  browser: 'browser',
+  server: 'server',
+  browserWorkerApp: 'browserWorkerApp',
+  browserWorkerUi: 'browserWorkerUi',
+} as const;
